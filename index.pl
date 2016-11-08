e5779a90b38cfb843f2bc0cf187a148b564adc33
bronya2057 <bronya10@gmail.com>
2016-11-08 17:36:02 +0200

    delete irrelevant data

--- index.pl
+++ index.pl
@@ -4,11 +4,15 @@ print "Content-type:text/html\n\n";
 print <<EndOfHTML;
 <html><head><title>Perl Environment Variables</title></head>
 <body>
-<h1>Perl Environment Variables</h1>
+<h1>1. Number of users on the website</h1>
+<h1>2. Number of users in the last hour</h1>
+<h1>3. Number of users in the last 24 hours</h1>
+<h1>4. 10 last useragents</h1>
+<h1>5. App on-line</h1>
+<h1>6. Query counter</h1>
+<b>7. Depending on the domain bold the appropriate task</b> 
 EndOfHTML
 
-foreach $key (sort(keys %ENV)) {
-  print "$key = $ENV{$key}<br>\n";
-}
+
 
 print "</body></html>";
